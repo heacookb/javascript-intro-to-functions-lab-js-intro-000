8a76280427b5f412746f138bcc950095e6c2f62c
Brian Heacook <heacookb@protonmail.com>
2018-01-01 19:30:28 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,11 +1,8 @@
 
 describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function shout(string)
-      return.string.toUpperCase()
-}
-  {
+  it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
-  
+    return string.toUpperCase()
   })
 })
 
